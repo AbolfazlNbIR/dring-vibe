bdff964e79645efeaa7cd07b13067c6ef104cea4
Abolfazl <abolfazlnb8813@gmail.com>
2026-05-28 11:14:01 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,15 +23,21 @@ python3 -m venv "$PROJECT_DIR/venv" > /dev/null 2>&1
 
 source "$PROJECT_DIR/venv/bin/activate"
 
-pip install --break-system-packages -r "$PROJECT_DIR/requirements.txt" > /dev/null 2>&1
+"$PROJECT_DIR/venv/bin/pip" install 
+--break-system-packages 
+-r "$PROJECT_DIR/requirements.txt" \
+
+> /dev/null 2>&1
 
 chmod +x "$PROJECT_DIR/dringvibe.py"
 
 sudo rm -f /usr/local/bin/dring-vibe
 
-echo '#!/bin/bash' | sudo tee /usr/local/bin/dring-vibe > /dev/null
-echo "cd "$PROJECT_DIR"" | sudo tee -a /usr/local/bin/dring-vibe > /dev/null
-echo "exec "$PROJECT_DIR/venv/bin/python3" "$PROJECT_DIR/dringvibe.py" "$@"" | sudo tee -a /usr/local/bin/dring-vibe > /dev/null
+sudo tee /usr/local/bin/dring-vibe > /dev/null << EOF
+#!/bin/bash
+cd "$PROJECT_DIR"
+exec "$PROJECT_DIR/venv/bin/python3" "$PROJECT_DIR/dringvibe.py" "$@"
+EOF
 
 sudo chmod +x /usr/local/bin/dring-vibe
 
